45e2d5bf08f886e4ef9d43a5bb71d063aa8879ba
viadel13 <shababizetre@gmail.com>
2023-07-24 19:38:39 +0100

    first commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
-    cors: "*",
+    cors: "*"
 })
 
 io.on('connection', (socket) => {
